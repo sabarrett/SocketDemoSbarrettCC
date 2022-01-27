04c3bc1ff4968e2b94ccba9bade426ce4b81c6d4
Sardonic <scottbarrett99@gmail.com>
2022-01-27 15:35:32 -0500

    Initial commit

---
+++ RoboCat/Src/Main.cpp
@@ -0,0 +1,24 @@
+
+#include "RoboCatPCH.h"
+
+#if _WIN32
+int main(int argc, const char** argv)
+{
+	UNREFERENCED_PARAMETER(argc);
+	UNREFERENCED_PARAMETER(argv);
+#else
+const char** __argv;
+int __argc;
+int main(int argc, const char** argv)
+{
+	__argc = argc;
+	__argv = argv;
+#endif
+
+	SocketUtil::StaticInit();
+
+	SocketUtil::CleanUp();
+
+	return 0;
+}
+
