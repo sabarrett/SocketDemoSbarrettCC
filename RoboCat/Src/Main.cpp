9a8dd8a700f9bc3b1d6c4a8d9130cd951a97de62
BradleyC <bradleycham234@gmail.com>
2022-02-16 22:33:20 -0500

    adding work

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -7,130 +7,173 @@
 #include <sstream>
 
 
-
-void DoTcpServer()
+void BradsTotallyOriginalServer()
 {
-	// Open a TCP socket
-	TCPSocketPtr listenSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
-	if (listenSocket == nullptr)
+	// create a socket to listen for input data
+	TCPSocketPtr listeningSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+	// check if the socket was created, if not kill the program
+	if (listeningSocket == nullptr)
 	{
-		SocketUtil::ReportError("Creating listen socket");
+		SocketUtil::ReportError("Could not create socket: Null Ptr");
+		ExitProcess(1); // kill
+	}
+	LOG("%s", "Listening socket created");
+
+	//see if this will get the port from the command line
+	//std::string port = StringUtils::GetCommandLineArg(1);
+	//LOG("%s", "WOWZA! You just told me: " + port)
+	// 
+	//create a prt for the port
+	SocketAddressPtr addressPtr = SocketAddressFactory::CreateIPv4FromString("0.0.0.0:8080"); // a hard-coded port to bind to
+	if (addressPtr == nullptr)
+	{
+		SocketUtil::ReportError("Could not create server address");
+		ExitProcess(1); // kill
+	}
+
+	LOG("%s", "WOW! You made an address for your socket");
+
+	//Bind Time!
+	bool bindError = listeningSocket->Bind(*addressPtr) != NO_ERROR;
+	if (bindError)
+	{
+		SocketUtil::ReportError("Socket could not bind to address");
+		ExitProcess(1); // kill
+	}
+
+	LOG("%s", "The binding is complete");
+
+	bool listenError = listeningSocket->Listen() != NO_ERROR;
+	if (listenError)
+	{
+		SocketUtil::ReportError("Listen Error");
+		ExitProcess(1); // kill
+	}
+
+	LOG("%s", "We're listening for incoming connections");
+
+	// accept is a blocking function so it will halt the program execution until it recieves a incoming connection
+
+	LOG("%s", "Waiting to accept connections.oof.oof.oof");
+
+	SocketAddress incomingAddress;
+	TCPSocketPtr incomingSocket = listeningSocket->Accept(incomingAddress);
+	while (incomingSocket == nullptr) // if it didnt work the first time, try again
+	{
+		incomingSocket = listeningSocket->Accept(incomingAddress);
+	}
+	
+	// CONECTION ACCEPTED WE HAVE CONTACT ==============================
+	LOG("Bleep Bloop...Accepted connection from: %s", incomingAddress.ToString().c_str());
+
+	//attempt to connect back to the client
+	/*
+	if (incomingSocket->Connect(incomingAddress) != NO_ERROR)
+	{
+		SocketUtil::ReportError("Connecting to server");
 		ExitProcess(1);
 	}
+	*/
 
-	LOG("%s", "Created listen socket");
+	//send data
+	std::thread t1([&]()
+		{
+			std::string msg("");
+			while (msg != "/exit")
+			{
+				std::getline(std::cin, msg);
+				incomingSocket->Send(msg.c_str(), msg.length());
+			}
+		});
 
-	//SocketAddress a2(INADDR_LOOPBACK, 8080);
-	// Listen only for connections on this machine
-	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8080");
-	if (a == nullptr)
-	{
-		SocketUtil::ReportError("Creating server address");
-		ExitProcess(1);
-	}
-
-	LOG("%s", "Create socket address");
-
-	// "Bind" that socket to the address we want to listen on
-	if (listenSocket->Bind(*a) != NO_ERROR)
-	{
-		SocketUtil::ReportError("Binding socket");
-		ExitProcess(1);
-	}
-
-	LOG("%s", "Bound socket");
-
-	// Call "Listen" to have the OS listen for connections on that socket
-	if (listenSocket->Listen() != NO_ERROR)
-	{
-		SocketUtil::ReportError("Listening");
-		ExitProcess(1);
-	}
-
-	LOG("%s", "Socket listening");
-
-	// Call "Accept" which *blocks* until we get a request to connect,
-	// and then it accepts that connection!
-
-
-	/// listenSocket <----- packets from other hosts wanting to connect
-	/// listenSocket -----> accepts connection, spawns:
-	///   connectionSocket <----> otherHost1
-	///   connectionSock2  <----> otherHost2
-
-	SocketAddress connAddr;
-	TCPSocketPtr conn;
-
-	//while (conn == nullptr)
-	{
-		conn = listenSocket->Accept(connAddr);
-	}
-
-	// This code isn't blocking anymore -- it'll run to the end of the program.
-
+	//receive data
 	char buffer[4096];
-	int32_t bytesRead = conn->Receive(buffer, 4096);
-
-	std::string msgReceived(buffer, bytesRead);
-	LOG("Received message: %s", msgReceived.c_str());
-
+	int32_t bytesReceived = incomingSocket->Receive(buffer, 4096);
+	while (bytesReceived < 4096)
+	{
+		bytesReceived = incomingSocket->Receive(buffer, 4096);
+		std::string receivedMsg(buffer, bytesReceived);
+		std::cout << receivedMsg << std::endl;
+		//LOG("%s: %s", incomingAddress.ToString().c_str(), receivedMsg.c_str());		
+	}
 }
 
-void DoTcpClient()
+void BradsLessOriginalClient()
 {
-	std::thread t(DoTcpServer);
-	//t.join();
-	// Open a TCP socket
-	TCPSocketPtr connSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
-	if (connSocket == nullptr)
+	TCPSocketPtr clientSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+	if (clientSocket == nullptr)
 	{
-		SocketUtil::ReportError("Creating listen socket");
+		SocketUtil::ReportError("Could not create client socket");
 		ExitProcess(1);
 	}
 
-	LOG("%s", "Created client socket");
+	LOG("%s", "Client socket created");
 
-	//SocketAddress a2(INADDR_LOOPBACK, 8080);
-	// Listen only for connections on this machine
-	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8081");
-	if (a == nullptr)
+	// Bind() - "Bind" socket -> tells OS we want to use a specific address
+	std::string port;
+	port = StringUtils::GetCommandLineArg(2);
+	std::string address = StringUtils::Sprintf("127.0.0.1:%s", port.c_str());
+	//std::string address = StringUtils::Sprintf("127.0.0.1:8081");
+	SocketAddressPtr clientAddress = SocketAddressFactory::CreateIPv4FromString(address.c_str());
+	if (clientAddress == nullptr)
+	{
+		SocketUtil::ReportError("Creating client address");
+		ExitProcess(1);
+	}
+
+	if (clientSocket->Bind(*clientAddress) != NO_ERROR)
+	{
+		SocketUtil::ReportError("Binding client socket");
+		// This doesn't block!
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Bound client socket");
+
+	// Connect() -> Connect socket to remote host
+
+	SocketAddressPtr serverAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8080");
+	if (serverAddress == nullptr)
 	{
 		SocketUtil::ReportError("Creating server address");
 		ExitProcess(1);
 	}
 
-	LOG("%s", "Create socket address");
-
-	// "Bind" that socket to the address we want to listen on
-	if (connSocket->Bind(*a) != NO_ERROR)
-	{
-		SocketUtil::ReportError("Binding socket");
-		ExitProcess(1);
-	}
-
-	LOG("%s", "Bound socket");
-
-	SocketAddressPtr servAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8080");
-	if (servAddress == nullptr)
-	{
-		SocketUtil::ReportError("Creating server address");
-		ExitProcess(1);
-	}
-
-	if (connSocket->Connect(*servAddress) != NO_ERROR)
+	if (clientSocket->Connect(*serverAddress) != NO_ERROR)
 	{
 		SocketUtil::ReportError("Connecting to server");
 		ExitProcess(1);
 	}
 
 	LOG("%s", "Connected to server!");
+	//CONECTION CREATED WE HAVE CONTACT ==============================
+	std::string msg("Hello You are connected!");
+	clientSocket->Send(msg.c_str(), msg.length());
 
-	std::string msg("Hello, server! How are you today?");
-	connSocket->Send(msg.c_str(), msg.length());
+	std::thread t([&]()
+		{
+			char buffer[4096];
+			int32_t bytesReceived = clientSocket->Receive(buffer, 4096);
+			while (bytesReceived < 4096)
+			{
+				bytesReceived = clientSocket->Receive(buffer, 4096);
+				std::string receivedMsg(buffer, bytesReceived);
+				std::cout << receivedMsg << std::endl;
+			}
+		});
 
-	LOG("%s", "Sent message to server");
+	//OutputWindow win;
 
+	msg = "";
+	while (msg != "/exit")
+	{
+		std::getline(std::cin, msg);
+		clientSocket->Send(msg.c_str(), msg.length());
+		//std::this_thread::sleep_for(std::chrono::milliseconds(250));
+	}
 }
+
+
 
 #if _WIN32
 int main(int argc, const char** argv)
@@ -155,16 +198,16 @@ int main(int argc, const char** argv)
 	//    (Actually, we want Peer-To-Peer, but we'll get there!)
 
 	// Server Code --------------
-	bool isServer = StringUtils::GetCommandLineArg(1) == "server";
-	if (isServer)
+	bool isServer = StringUtils::GetCommandLineArg(1) == "server"; // check if the command on the executable is 'server'
+	if (isServer) // if it is the server
 	{
-		DoTcpServer();
+		BradsTotallyOriginalServer();	
 	}
 
 	// Client Code --------------
 	if (!isServer)
 	{
-		DoTcpClient();
+		BradsLessOriginalClient();
 	}
 
 	SocketUtil::CleanUp();
