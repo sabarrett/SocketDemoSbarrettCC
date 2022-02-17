26a2d197ffa9605b112b29fc2a713fa4521f594a
Brandon Boras <brandon.boras@mymail.champlain.edu>
2022-02-17 13:22:15 -0500

    first stretch of work

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -6,9 +6,167 @@
 #include <string>
 #include <sstream>
 
+
+/// <summary>
+/// Based from the DoTcpServer() in the in-class demo
+/// </summary>
+void SetUpListening()
+{
+	// Open a TCP socket
+	TCPSocketPtr listenSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+	if (listenSocket == nullptr)
+	{
+		SocketUtil::ReportError("Creating listen socket");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Created listen socket");
+
+	//SocketAddress a2(INADDR_LOOPBACK, 8080);
+	// Listen only for connections on this machine
+	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString("0.0.0.0:8080");
+	if (a == nullptr)
+	{
+		SocketUtil::ReportError("Creating server address");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Create socket address");
+
+	// "Bind" that socket to the address we want to listen on
+	if (listenSocket->Bind(*a) != NO_ERROR)
+	{
+		SocketUtil::ReportError("Binding socket");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Bound socket");
+
+	// Call "Listen" to have the OS listen for connections on that socket
+	if (listenSocket->Listen() != NO_ERROR)
+	{
+		SocketUtil::ReportError("Listening");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Socket listening");
+
+	// Call "Accept" which *blocks* until we get a request to connect,
+	// and then it accepts that connection!
+
+
+	/// listenSocket <----- packets from other hosts wanting to connect
+	/// listenSocket -----> accepts connection, spawns:
+	///   connectionSocket <----> otherHost1
+	///   connectionSock2  <----> otherHost2
+
+	SocketAddress connAddr;
+	TCPSocketPtr conn;
+
+	TCPSocketPtr connSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+	if (connSocket == nullptr)
+	{
+		SocketUtil::ReportError("Creating listen socket");
+		ExitProcess(1);
+	}
+
+	while (conn == nullptr)
+	{
+		LOG("%s", "Waiting For Connection");
+
+		conn = listenSocket->Accept(connAddr);
+
+
+		SocketAddressPtr servAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8080");
+		if (servAddress == nullptr)
+		{
+			SocketUtil::ReportError("Creating server address");
+			ExitProcess(1);
+		}
+
+		if (connSocket->Connect(*servAddress) != NO_ERROR)
+		{
+			SocketUtil::ReportError("Connecting to server");
+			ExitProcess(1);
+		}
+
+		LOG("%s", "Connected to server!");
+
+		std::string msg("Hello, server! How are you today?");
+		connSocket->Send(msg.c_str(), msg.length());
+
+		LOG("%s", "Sent message to server");
+	}
+
+	// This code isn't blocking anymore -- it'll run to the end of the program.
+
+	char buffer[4096];
+	int32_t bytesRead = conn->Receive(buffer, 4096);
+
+	std::string msgReceived(buffer, bytesRead);
+	LOG("Received message: %s", msgReceived.c_str());
+}
+
+/// <summary>
+/// Based on the DoTcpClient() in the in-class demo
+/// </summary>
+void SetUpSending()
+{
+	//std::thread t(DoTcpServer);
+	//t.join();
+	// Open a TCP socket
+	TCPSocketPtr connSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+	if (connSocket == nullptr)
+	{
+		SocketUtil::ReportError("Creating listen socket");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Created client socket");
+
+	//SocketAddress a2(INADDR_LOOPBACK, 8080);
+	// Listen for connections from any machine
+	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString("0.0.0.0:8081");
+	if (a == nullptr)
+	{
+		SocketUtil::ReportError("Creating server address");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Create socket address");
+
+	// "Bind" that socket to the address we want to listen on
+	if (connSocket->Bind(*a) != NO_ERROR)
+	{
+		SocketUtil::ReportError("Binding socket");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Bound socket");
+
+	SocketAddressPtr servAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8080");
+	if (servAddress == nullptr)
+	{
+		SocketUtil::ReportError("Creating server address");
+		ExitProcess(1);
+	}
+
+	if (connSocket->Connect(*servAddress) != NO_ERROR)
+	{
+		SocketUtil::ReportError("Connecting to server");
+		ExitProcess(1);
+	}
+
+	LOG("%s", "Connected to server!");
+
+	std::string msg("Hello, server! How are you today?");
+	connSocket->Send(msg.c_str(), msg.length());
+
+	LOG("%s", "Sent message to server");
+}
+
+
 #if _WIN32
-
-
 int main(int argc, const char** argv)
 {
 	UNREFERENCED_PARAMETER(argc);
@@ -25,6 +183,10 @@ int main(int argc, const char** argv)
 	SocketUtil::StaticInit();
 
 	OutputWindow win;
+
+	std::thread t_Listening(SetUpListening);
+	//std::thread t_Sending(SetUpSending);
+	
 
 	std::thread t([&win]()
 				  {
@@ -46,7 +208,8 @@ int main(int argc, const char** argv)
 		std::getline(std::cin, input);
 		win.WriteFromStdin(input);
 	}
-
+	t_Listening.join();
+	//t_Sending.join();
 	SocketUtil::CleanUp();
 
 	return 0;
