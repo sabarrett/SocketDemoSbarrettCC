d5cf550f659a044eaefca86034e6ba8cbee28fb6
Brandon L'Abbe <brandon.labbe@mymail.champlain.edu>
2022-02-17 23:53:54 -0500

    Final Commit

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -8,7 +8,20 @@
 
 void DoTcpServer();
 void DoTcpClient();
+
 void sendMessage(TCPSocketPtr socket, string message); 
+string receiveMessage(TCPSocketPtr socket);
+
+TCPSocketPtr establishTCPSocketUsingIPV4(string address, int port); //Create a TCPSocket and Bind it to a given address and port
+void connectTCPSocketUsingIPV4(TCPSocketPtr socket, string address, int port);
+TCPSocketPtr listenAndAcceptTCPConnection(TCPSocketPtr socket);
+
+bool AOLSend(TCPSocketPtr socket, string username, OutputWindow win);
+bool AOLReceive(TCPSocketPtr socket, OutputWindow win);
+void AOLReceiveLoop(TCPSocketPtr socket, OutputWindow win);
+
+void write(OutputWindow win, string msg);
+
 
 using namespace std;
 
@@ -53,46 +66,167 @@ int main(int argc, const char** argv)
 
 void DoTcpServer()
 {
+	system("cls");
 
-	cout << "Welcome to the AOL!" << endl;
-	cout << "Please enter your username: ";
+
+	OutputWindow win;
+
+	write(win, "Welcome to the AOL!");
+	cout << "Please enter your username: " << endl;
 	
-	string username;
+	string username, msg;
 
 	cin >> username;
 
+
 	// Open a TCP socket
-	TCPSocketPtr listenSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
-	if (listenSocket == nullptr)
+	TCPSocketPtr listenSocket = establishTCPSocketUsingIPV4("0.0.0.0", 8080);
+	
+
+	write(win, "Bound socket");
+	write(win, "Please type /exit to quit!");
+
+	// Call "Listen" to have the OS listen for connections on that socket
+	
+	TCPSocketPtr conn = listenAndAcceptTCPConnection(listenSocket);
+
+	sendMessage(conn, username + " has joined the channel!");
+
+	cin.ignore(); 
+
+	thread recThread(AOLReceiveLoop, conn, win);
+
+	bool shouldContinue = true;
+
+	while (shouldContinue)
+	{
+		shouldContinue = AOLSend(conn, username, win);
+	}
+	
+	recThread.join();
+
+	/*std::string msgReceived = receiveMessage(conn);
+	LOG("Received message: %s", msgReceived.c_str());
+
+	std::string msg("Hi Client! What a swell day!");
+	sendMessage(conn, msg);*/
+}
+
+void DoTcpClient()
+{
+	system("cls");
+
+
+	OutputWindow win;
+
+	write(win, "Welcome to the AOL!");
+	cout << "Please enter your username: " << endl;
+
+	string username, msg;
+
+	cin >> username;
+	
+	TCPSocketPtr connSocket = establishTCPSocketUsingIPV4("127.0.0.1", 8085);
+
+	connectTCPSocketUsingIPV4(connSocket, "127.0.0.1", 8080);
+
+	write(win, "Connected to server!");
+	write(win, "Please type /exit to quit!");
+
+	sendMessage(connSocket, username + " has joined the channel!");
+
+	cin.ignore();
+
+	thread recThread(AOLReceiveLoop, connSocket, win);
+
+	bool shouldContinue = true;
+
+	while (shouldContinue)
+	{
+		shouldContinue = AOLSend(connSocket, username, win);
+	}
+	
+	recThread.join();
+
+	/*string msg("Hello, server! How are you today?");
+	sendMessage(connSocket, msg);
+
+	LOG("%s", "Sent message to server");
+
+	string msgReceived = receiveMessage(connSocket);
+	
+	LOG("Received message: %s", msgReceived.c_str());*/
+}
+
+void sendMessage(TCPSocketPtr socket, string message)
+{
+	socket->Send(message.c_str(), message.length());
+}
+
+string receiveMessage(TCPSocketPtr socket)
+{
+	char buffer[4096];
+	int32_t bytesRead = socket->Receive(buffer, 4096);
+
+	string msgReceived(buffer, bytesRead);
+
+	return msgReceived;
+}
+
+TCPSocketPtr establishTCPSocketUsingIPV4(string address, int port)
+{
+	// Open a TCP socket
+	TCPSocketPtr connSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+	if (connSocket == nullptr)
 	{
 		SocketUtil::ReportError("Creating listen socket");
 		ExitProcess(1);
 	}
 
-	LOG("%s", "Created listen socket");
 
 	//SocketAddress a2(INADDR_LOOPBACK, 8080);
 	// Listen only for connections on this machine
-	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString("0.0.0.0:8080");
+
+	string myAddress = address + ":" + to_string(port);
+
+	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString(myAddress);
 	if (a == nullptr)
 	{
 		SocketUtil::ReportError("Creating server address");
 		ExitProcess(1);
 	}
 
-	LOG("%s", "Create socket address");
 
 	// "Bind" that socket to the address we want to listen on
-	if (listenSocket->Bind(*a) != NO_ERROR)
+	if (connSocket->Bind(*a) != NO_ERROR)
 	{
 		SocketUtil::ReportError("Binding socket");
 		ExitProcess(1);
 	}
 
-	LOG("%s", "Bound socket");
+	return connSocket;
+}
 
-	// Call "Listen" to have the OS listen for connections on that socket
-	if (listenSocket->Listen() != NO_ERROR)
+void connectTCPSocketUsingIPV4(TCPSocketPtr socket, string address, int port)
+{
+	string connAddress = address + ":" + to_string(port);
+	SocketAddressPtr b = SocketAddressFactory::CreateIPv4FromString(connAddress);
+	if (b == nullptr)
+	{
+		SocketUtil::ReportError("Creating server address");
+		ExitProcess(1);
+	}
+
+	if (socket->Connect(*b) != NO_ERROR)
+	{
+		SocketUtil::ReportError("Connecting to server");
+		ExitProcess(1);
+	}
+}
+
+TCPSocketPtr listenAndAcceptTCPConnection(TCPSocketPtr socket)
+{
+	if (socket->Listen() != NO_ERROR)
 	{
 		SocketUtil::ReportError("Listening");
 		ExitProcess(1);
@@ -113,83 +247,56 @@ void DoTcpServer()
 	TCPSocketPtr conn;
 
 	//while (conn == nullptr)
-	{
-		conn = listenSocket->Accept(connAddr);
-	}
 
-	// This code isn't blocking anymore -- it'll run to the end of the program.
+	conn = socket->Accept(connAddr);
 
-	char buffer[4096];
-	int32_t bytesRead = conn->Receive(buffer, 4096);
-
-	std::string msgReceived(buffer, bytesRead);
-	LOG("Received message: %s", msgReceived.c_str());
-
-	std::string msg("Hi Client! What a swell day!");
-	conn->Send(msg.c_str(), msg.length());
+	return conn;
 }
 
-void DoTcpClient()
+bool AOLSend(TCPSocketPtr socket, string username, OutputWindow win)
 {
-
-	cout << "Welcome to the AOL!" << endl;
-	cout << "Please enter your username: ";
-
-	string username;
-
-	cin >> username;
+	string msg;
+	cout << username << ": ";
+	getline(cin, msg);
+	write(win, username + ": " + msg);
+	win.Scroll(-1);
 
 
-	// Open a TCP socket
-	TCPSocketPtr connSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
-	if (connSocket == nullptr)
+	if (msg == "/exit")
 	{
-		SocketUtil::ReportError("Creating listen socket");
-		ExitProcess(1);
+		sendMessage(socket, "/exit");
+		return false;
 	}
 
+	sendMessage(socket, username + ": " + msg);
+	return true;
+}
 
-	//SocketAddress a2(INADDR_LOOPBACK, 8080);
-	// Listen only for connections on this machine
-	SocketAddressPtr a = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8081");
-	if (a == nullptr)
+bool AOLReceive(TCPSocketPtr socket, OutputWindow win)
+{
+	string msg;
+	msg = receiveMessage(socket);
+
+	if (msg == "/exit")
 	{
-		SocketUtil::ReportError("Creating server address");
-		ExitProcess(1);
+		return false;
 	}
 
+	write(win, msg);
+	return true;
+}
 
-	// "Bind" that socket to the address we want to listen on
-	if (connSocket->Bind(*a) != NO_ERROR)
+void AOLReceiveLoop(TCPSocketPtr socket, OutputWindow win)
+{
+	bool shouldContinue = true;
+	while (shouldContinue)
 	{
-		SocketUtil::ReportError("Binding socket");
-		ExitProcess(1);
+		shouldContinue = AOLReceive(socket, win);
 	}
+}
 
-
-	SocketAddressPtr servAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:8080");
-	if (servAddress == nullptr)
-	{
-		SocketUtil::ReportError("Creating server address");
-		ExitProcess(1);
-	}
-
-	if (connSocket->Connect(*servAddress) != NO_ERROR)
-	{
-		SocketUtil::ReportError("Connecting to server");
-		ExitProcess(1);
-	}
-
-	LOG("%s", "Connected to server!");
-
-	std::string msg("Hello, server! How are you today?");
-	connSocket->Send(msg.c_str(), msg.length());
-
-	LOG("%s", "Sent message to server");
-
-	char buffer[4096];
-	int32_t bytesRead = connSocket->Receive(buffer, 4096);
-
-	std::string msgReceived(buffer, bytesRead);
-	LOG("Received message: %s", msgReceived.c_str());
+void write(OutputWindow win, string msg)
+{
+	win.Write(msg);
+	win.Scroll(-1);
 }
