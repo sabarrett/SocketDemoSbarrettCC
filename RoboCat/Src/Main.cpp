6db7fe21200749d3075fd717511470f6ea6c3cb5
Olli Machina <olli.machina@mymail.champlain.edu>
2022-02-25 14:13:06 -0500

    adding work from class in main

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -1,6 +1,8 @@
 
 #include "RoboCatPCH.h"
 
+#include <thread>;
+
 #if _WIN32
 
 
@@ -18,6 +20,49 @@ int main(int argc, const char** argv)
 #endif
 
 	SocketUtil::StaticInit();
+
+	UDPSocketPtr cliSock = SocketUtil::CreateUDPSocket(SocketAddressFamily::INET);
+	UDPSocketPtr srvSock = SocketUtil::CreateUDPSocket(SocketAddressFamily::INET);
+
+	SocketAddressPtr srvAddr = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:9001");
+
+	//clientAddr not needed for long, don't keep it around
+	{
+		SocketAddressPtr cliAddr = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:9000");
+		if (cliAddr == nullptr)
+		{
+			SocketUtil::ReportError("create client socket");
+			ExitProcess(1);
+		}
+		cliSock->Bind(*cliAddr);
+	}
+	srvSock->Bind(*srvAddr);
+
+	std::string msg("Hello Server");
+	int nBytesSent = cliSock->SendTo(msg.c_str(), msg.length(), *srvAddr);
+	if (nBytesSent <= 0)
+	{
+		SocketUtil::ReportError("Client SendTo");
+	}
+	std::cout << "Sent " << nBytesSent << " bytes\n";
+	
+
+	std::thread srvThread([&srvSock]()
+	{
+		char buffer[4096];
+		SocketAddress fromAddr;
+		int nBytesRecieved = srvSock->ReceiveFrom(buffer, 4096, fromAddr);
+		if (nBytesRecieved <= 0)
+		{
+			SocketUtil::ReportError("Server RecieveFrom");
+			return;
+		}
+		std::string msg(buffer, nBytesRecieved);
+		std::cout << "Recieved message from: " << fromAddr.ToString() << ": " << msg << std::endl;
+	});
+
+
+	srvThread.join();
 
 	SocketUtil::CleanUp();
 
