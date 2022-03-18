d41889968437b492833ede64a4468c79853237b9
Matt <matthew.esslie@mymail.champlain.edu>
2022-03-18 16:02:26 -0400

    adding notes and plans for the project

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -1,5 +1,13 @@
 
 #include "RoboCatPCH.h"
+#include "InputSystem.h"
+#include "GraphicsLibrary.h"
+#include "Colour.h"
+
+//Some functions to make shapes for the game objects to track and send info about
+//al_draw_circle
+//al_draw_triangle
+//al_draw_rectangle
 
 #if _WIN32
 
@@ -20,6 +28,15 @@ int main(int argc, const char** argv)
 	SocketUtil::StaticInit();
 
 	//while (activeConnection == false)
+	InputSystem mInputSystem; 
+	bool activeConnection; // = true
+
+	//something to start up the connection (info from assignment 1?)
+	//then add the keys for making the objects spawn (3 objects, 3 keys)
+		//need something to track the objects made? idk if there is something i can use?
+	//each update send out the information about the object type and its position
+		//then prepare to receive it as well?
+	
 	
 	SocketUtil::CleanUp();
 
