0b1c4a5a9bc7287b457a46d8925e45c5bdc14398
Bradley Chamberlain <bradleycham234@gmail.com>
2022-03-27 16:20:24 -0400

    Adding allegro linking

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -1,6 +1,6 @@
 
 #include "RoboCatPCH.h"
-
+#include "allegro.h"
 #if _WIN32
 
 
@@ -17,9 +17,24 @@ int main(int argc, const char** argv)
 	__argv = argv;
 #endif
 
-	SocketUtil::StaticInit();
+	
 
-	SocketUtil::CleanUp();
+	ALLEGRO_DISPLAY* display;
+	if (!al_init())
+	{
+		printf("fuck");
+	}
+	display = al_create_display(800, 600);
+
+	system("pause");
+
+	//socket stuff here
+	SocketUtil::StaticInit(); // socket initialization
+
+	//create a server
+	UDPSocketPtr serverSocket;
+
+	SocketUtil::CleanUp(); // socket cleanup
 
 	return 0;
 }
