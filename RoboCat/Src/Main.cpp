0314041583c32539b9b54dbadadc62bd961fff9d
GoGetAVegan <maximus.lorah@mymail.champlain.edu>
2022-03-28 20:00:28 -0400

    Transform Struct

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -17,13 +17,17 @@ static float RandomFloat(float min, float max);
 static void InitGame();
 static void UpdateGame();
 
+struct Transform {
+    Vector2 size;
+    Vector2 velocity;
+
+};
 struct Player {
     Vector2 position;
     Vector2 size;
     Color playerColor;
     int id;
     int score;
-    int life;
 };
 
 struct Ball {
@@ -46,8 +50,10 @@ struct Brick {
         return Rectangle{ position.x - bricks.x / 2, position.y - bricks.y / 2, screenWidth / BRICK_COLUMNS, 40 };
     }
 };
+
 static Brick brickList[BRICK_ROWS][BRICK_COLUMNS] = { 0 };
 static Player player;
+static Player player2;
 static Ball ball;
 
 enum class StartupScreenAction {
@@ -266,7 +272,7 @@ int main(int argc, const char** argv)
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
-    SocketAddress address;
+   /* SocketAddress address;
     StartupScreenAction action = StartupScreen(&address);
 
     if (action == StartupScreenAction::QUIT) {
@@ -281,59 +287,52 @@ int main(int argc, const char** argv)
         socket = StartHost();
 
     if (action == StartupScreenAction::CONNECT)
-       socket = StartConnect(address);
+       socket = StartConnect(address);*/
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        UpdateGame();
+        //----------------------------------------------------------------------------------
 
-        if (player.life > 0)
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        //Draw Ball
+        if (ball.isDead == false)
         {
-            // Update
-            //----------------------------------------------------------------------------------
-            // TODO: Update your variables here
-            UpdateGame();
-            //----------------------------------------------------------------------------------
+            DrawCircleV(ball.position, ball.radius, ball.ballColor);
+        }
 
-            // Draw
-            //----------------------------------------------------------------------------------
-            BeginDrawing();
+        //Draw Player
+        DrawRectangle(player.position.x - player.size.x / 2, player.position.y - player.size.y / 2, player.size.x, player.size.y, player.playerColor);
 
-            ClearBackground(RAYWHITE);
-
-            //Draw Ball
-            if (ball.isDead == false)
+        //Draw Bricks
+        for (int i = 0; i < BRICK_ROWS; i++)
+        {
+            for (int j = 0; j < BRICK_COLUMNS; j++)
             {
-                DrawCircleV(ball.position, ball.radius, ball.ballColor);
-            }
-
-            //Draw Player
-            DrawRectangle(player.position.x - player.size.x / 2, player.position.y - player.size.y / 2, player.size.x, player.size.y, player.playerColor);
-
-            //Draw Bricks
-            for (int i = 0; i < BRICK_ROWS; i++)
-            {
-                for (int j = 0; j < BRICK_COLUMNS; j++)
+                if (brickList[i][j].isDead == false)
                 {
-                    if (brickList[i][j].isDead == false)
-                    {
-                        DrawRectangleRec(brickList[i][j].getRectangle(), brickList[i][j].brickColor);
-                    }
+                    DrawRectangleRec(brickList[i][j].getRectangle(), brickList[i][j].brickColor);
                 }
             }
         }
 
         ClearBackground(RAYWHITE);
-
-        if (player.life <= 0)
-        {
-            string playerScore = "Score: " + std::to_string(player.score);
-            DrawText(playerScore.c_str(), GetScreenWidth() / 2 - MeasureText(playerScore.c_str(), 40)/2, GetScreenHeight() / 2 - 50, 40, BLACK);
-        }
+        string playerScore = "Score: " + std::to_string(player.score);
+        DrawText(playerScore.c_str(), 0, GetScreenHeight() / 2 + 100, 40, BLACK);
+        string playerScore2 = "Score: " + std::to_string(player2.score);
+        DrawText(playerScore2.c_str(), screenWidth-100, GetScreenHeight() / 2 + 100, 40, BLACK);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
-
     }
 
     Shutdown();
@@ -347,12 +346,17 @@ void InitGame()
     //Setting bricks to screen
     bricks = { screenWidth/ BRICK_COLUMNS,40};
 
-    //Temp Init Player
+    //Init Player
     player.position = { screenWidth / 2, screenHeight * 7 / 8 };
     player.size = { screenWidth / 10, 20 };
     player.playerColor = { 0,0,255,255 };
     player.id = 0;
-    player.life = 3;
+
+    //Init Player 2
+    player2.position = { screenWidth / 2, screenHeight * 7 / 8 };
+    player2.size = { screenWidth / 10, 20 };
+    player2.playerColor = { 255,0,0,255 };
+    player2.id = 1;
 
     //Temp Init Ball
     ball.position = { screenWidth / 2, screenHeight * 7 / 10};
@@ -426,6 +430,8 @@ void UpdateGame()
         ball.velocity.x = (ball.position.x - player.position.x) / (player.size.x / 2) * 5;
     }
 
+    //if(CheckCollisionCircleRec)
+
     //Wall Collision
     if (ball.position.x + ball.radius >= screenWidth || ball.position.x - ball.radius <= 0)
     {
@@ -439,8 +445,6 @@ void UpdateGame()
     {
         ball.velocity = { 0,0 };
         ball.isDead = true;
-
-        player.life--;
     }
 
     //Player Movement
