09afd2648703b2d6d99466e40fa064f6780bc285
Matthew Esslie <matthew.esslie@mymail.champlain.edu>
2022-04-19 12:03:17 -0400

    trying to solve the pathing issues

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -8,7 +8,7 @@
 #include "NetworkManagerServer.h"
 
 const int MAX_OBJECT_COUNT = 100; //i think this might be needed i am unsure
-std::string BACKGROUND_PATH = "Woods.png"; //I think
+std::string BACKGROUND_PATH = "..\\..\\Assets\\Woods.png"; //I think
 
 #if _WIN32
 
